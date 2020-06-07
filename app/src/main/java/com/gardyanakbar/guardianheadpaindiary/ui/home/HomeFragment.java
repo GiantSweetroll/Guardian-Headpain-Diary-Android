72cdb98b22d80344bca781b82698f8f49c234a28
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2020-06-07 18:08:43 +0700

    Added items to home page, also resized the logo there

--- app/src/main/java/com/gardyanakbar/guardianheadpaindiary/ui/home/HomeFragment.java
+++ app/src/main/java/com/gardyanakbar/guardianheadpaindiary/ui/home/HomeFragment.java
@@ -1,28 +1,76 @@
 package com.gardyanakbar.guardianheadpaindiary.ui.home;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.text.HtmlCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.gardyanakbar.guardianheadpaindiary.R;
 import com.gardyanakbar.guardianheadpaindiary.constants.Globals;
+import com.gardyanakbar.guardianheadpaindiary.datadrivers.PainEntryData;
+import com.gardyanakbar.guardianheadpaindiary.methods.FileOperation;
+import com.gardyanakbar.guardianheadpaindiary.methods.Methods;
 
-public class HomeFragment extends Fragment {
+import java.util.List;
 
+import giantsweetroll.date.Date;
+
+public class HomeFragment extends Fragment
+{
+
+    //Fields
+    private static final String TAG = "HomeFragment";
     private HomeViewModel homeViewModel;
+    private TextView tvEntriesCount, tvSeeMore, tvMaxIntensity, tvMaxDuration, tvMaxDurationUnit, tvRecap;
+    private Button btnNewEntry;
+    //Constants
+    private final int SECONDS_INDEX = 0,
+            MINUTES_INDEX = 1,
+            HOURS_INDEX = 2,
+            DAYS_INDEX = 3;
 
+    //Private Methods
+    private String getDurationUnit(int index)
+    {
+        if (index == this.SECONDS_INDEX)
+        {
+            return this.getString(R.string.entry_log_form_duration_intensity_unit_seconds_text);
+        }
+        else if (index == this.MINUTES_INDEX)
+        {
+            return this.getString(R.string.entry_log_form_duration_intensity_unit_minutes_text);
+        }
+        else if (index == this.HOURS_INDEX)
+        {
+            return this.getString(R.string.entry_log_form_duration_intensity_unit_hours_text);
+        }
+        else if (index == this.DAYS_INDEX)
+        {
+            return this.getString(R.string.entry_log_form_duration_intensity_unit_days_text);
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    //Overridden Methods
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState)
     {
+        //Initialization
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 //        final TextView textView = root.findViewById(R.id.text_home);
@@ -33,5 +81,106 @@ public class HomeFragment extends Fragment {
 ////            }
 ////        });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Initialization
+        this.tvEntriesCount = view.findViewById(R.id.entriesCount);
+        this.tvSeeMore = view.findViewById(R.id.seeMore);
+        this.tvMaxIntensity = view.findViewById(R.id.maxIntensity);
+        this.tvMaxDuration = view.findViewById(R.id.maxDuration);
+        this.tvMaxDurationUnit = view.findViewById(R.id.durationUnit);
+        this.tvRecap = view.findViewById(R.id.todayRecap);
+        this.btnNewEntry = view.findViewById(R.id.btnNewEntry);
+
+        //Properties
+        this.tvSeeMore.setText(HtmlCompat.fromHtml(Methods.giveTextHTMLLinkStyle(this.getString(R.string.see_more_text)), HtmlCompat.FROM_HTML_MODE_LEGACY));
+        //Underlines the "Today's Recap" text
+        SpannableString content = new SpannableString(this.getString(R.string.home_today_recap_title));
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        this.tvRecap.setText(content);
+        this.tvEntriesCount.setText("0");
+        this.tvMaxIntensity.setText("0");
+        this.tvMaxDuration.setText("0");
+        this.tvSeeMore.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Globals.bottomNavigationView.setSelectedItemId(R.id.navigation_table);
+            }
+        });
+        this.btnNewEntry.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Globals.bottomNavigationView.setSelectedItemId(R.id.navigation_new_entry);
+            }
+        });
+
+        //Get entries from today
+        List<PainEntryData> entries = FileOperation.getListOfEntries(Globals.activePatient.getID(), new Date(), new Date());
+        int maxInt = 0;
+        long maxDur = 0;
+        //Update no of entries
+        this.tvEntriesCount.setText(Integer.toString(entries.size()));
+        //Get max intensity and duration
+        for (PainEntryData entry : entries)
+        {
+            int intensity = Integer.parseInt(entry.getIntensity());
+            long duration = Long.parseLong(entry.getDuration());
+
+            if (intensity > maxInt)
+            {
+                maxInt = intensity;
+            }
+            if (duration > maxDur)
+            {
+                maxDur = duration;
+            }
+        }
+        //Update max intensity
+        this.tvMaxIntensity.setText(Integer.toString(maxInt));
+        //Convert max duration to the appropriate units
+        String duration = Long.toString(maxDur);
+        try
+        {
+            int value = Integer.parseInt(duration);
+            int index = SECONDS_INDEX;
+
+            if (value >=86400d)								//Days
+            {
+                value = Methods.secondsToDays(value);
+                index = DAYS_INDEX;
+            }
+            else if (value>=3600d && value<86400d)	//Hours
+            {
+                value = Methods.secondsToHours(value);
+                index = HOURS_INDEX;
+            }
+            else if (value<3600d && value>=60d)		//Minutes
+            {
+                value = Methods.secondsToMinutes(value);
+                index = MINUTES_INDEX;
+            }
+            this.tvMaxDuration.setText(Integer.toString(value));
+            this.tvMaxDurationUnit.setText(this.getDurationUnit(index));
+        }
+        catch(NumberFormatException ex)
+        {
+            double val = Double.parseDouble(duration);
+            long value = (long)val;
+            value = value/86400;
+            int index = DAYS_INDEX;
+
+            this.tvMaxDuration.setText(Long.toString(value));
+            this.tvMaxDurationUnit.setText(this.getDurationUnit(index));
+        }
+        catch(Exception ex) {ex.printStackTrace();}
     }
 }
